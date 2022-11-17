9c8f43b682d5b7af2df13328d201169f01636986
rajesh.udhayan <70341492+rajesh-udhayan@users.noreply.github.com>
2022-11-17 22:11:45 +0530

    Refactoring. Moved game related code to game class.

--- app/src/main/java/com/anonymous/bowlingscore/Game.kt
+++ app/src/main/java/com/anonymous/bowlingscore/Game.kt
@@ -6,4 +6,20 @@ data class Game(
     val bonusRoll2: Int? = null
 ) {
 
+    private fun rollsFollowing(frameNumber: Int): List<Int> {
+        val remainingRolls = frames.drop(frameNumber + 1).flatMap { listOf(it.firstRoll, it.secondRoll) }
+        return (remainingRolls + bonusRoll1 + bonusRoll2).filterNotNull()
+    }
+
+    fun getBonusRolls(frameNumber: Int): Pair<Int?, Int?> {
+        val frame = frames[frameNumber]
+        val rollsFollowing = rollsFollowing(frameNumber)
+        val nextRoll = rollsFollowing.getOrNull(0)
+        val secondNextRoll = rollsFollowing.getOrNull(1)
+        return Pair(
+            nextRoll.takeIf { frame.let { it.isSpare || it.isStrike } },
+            secondNextRoll?.takeIf { frame.isStrike }
+        )
+    }
+
 }
--- app/src/main/java/com/anonymous/bowlingscore/ScoreCalculator.kt
+++ app/src/main/java/com/anonymous/bowlingscore/ScoreCalculator.kt
@@ -3,21 +3,17 @@ package com.anonymous.bowlingscore
 class ScoreCalculator {
 
     fun getFrameScore(frame: Frame, bonusRoll1: Int? = null, bonusRoll2: Int? = null): Int {
-        return frame.firstRoll+
+        return frame.firstRoll +
                 (frame.secondRoll ?: 0) +
-                (bonusRoll1 ?: 0)+
+                (bonusRoll1 ?: 0) +
                 (bonusRoll2 ?: 0)
     }
 
     fun getGameScore(game: Game): Int {
         var score = 0
         game.frames.forEachIndexed { frameNumber, frame ->
-            val remainingRolls = game.frames.drop(frameNumber + 1)
-                .flatMap { listOf(it.firstRoll, it.secondRoll) }
-            val rollsFollowing = (remainingRolls + game.bonusRoll1 + game.bonusRoll2).filterNotNull()
-            val nextRoll = rollsFollowing.getOrNull(0).takeIf { frame.let { it.isSpare || it.isStrike } }
-            val secondNextRoll =  rollsFollowing.getOrNull(1).takeIf { frame.isStrike }
-            score += getFrameScore(frame, nextRoll, secondNextRoll)
+            val (bonusRoll1, bonusRoll2) = game.getBonusRolls(frameNumber)
+            score += getFrameScore(frame, bonusRoll1, bonusRoll2)
         }
         return score
     }
