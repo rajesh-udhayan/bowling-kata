3b29a875e5f9264c1c2634dd43b970ec7ca353b9
rajesh.udhayan <70341492+rajesh-udhayan@users.noreply.github.com>
2022-11-17 22:50:55 +0530

    Minor refactoring.

--- app/src/test/java/com/anonymous/bowlingscore/ScoreSimulatorTest.kt
+++ app/src/test/java/com/anonymous/bowlingscore/ScoreSimulatorTest.kt
@@ -1,24 +1,29 @@
 package com.anonymous.bowlingscore
 
 import com.google.common.truth.Truth.*
+import org.junit.Before
 import org.junit.Test
 
 class ScoreSimulatorTest {
 
+    lateinit var scoreSimulator: ScoreSimulator
+
+    @Before
+    fun setUp(){
+        scoreSimulator = ScoreSimulator()
+    }
+
     @Test
     fun `should return 0 frames for new game`(){
-        val scoreSimulator = ScoreSimulator()
-
         assertThat(scoreSimulator.game.frames.count()).isEqualTo(0)
     }
 
     @Test
     fun `should add roll to the game`(){
-        val scoreSimulator = ScoreSimulator()
-
         scoreSimulator.addRoll(5)
 
         assertThat(scoreSimulator.game.frames.count()).isEqualTo(1)
         assertThat(scoreSimulator.game.frames[0]).isEqualTo(Frame(5, null))
     }
+
 }
