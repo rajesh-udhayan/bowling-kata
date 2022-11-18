8d09ef178624fc03fd9106f22b971a84d5c2f8fb
rajesh.udhayan <70341492+rajesh-udhayan@users.noreply.github.com>
2022-11-18 17:43:25 +0530

    Test passed for adding roll

--- app/src/main/java/com/anonymous/bowlingscore/MainViewModel.kt
+++ app/src/main/java/com/anonymous/bowlingscore/MainViewModel.kt
@@ -5,12 +5,20 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlin.random.Random
 
 @HiltViewModel
 class MainViewModel @Inject constructor(val simulator: ScoreSimulator) : ViewModel() {
 
     private var mGameState = MutableLiveData<Game>()
     val gameState: LiveData<Game> = mGameState
+
+    private val random = Random
+
+    fun addRoll(){
+        simulator.addRoll(random.nextInt(simulator.pinsRemaining + 1))
+        mGameState.postValue(simulator.game)
+    }
 
     fun startNewGame(){
         simulator.game = Game(emptyList())
--- app/src/test/java/com/anonymous/bowlingscore/MainViewModelTest.kt
+++ app/src/test/java/com/anonymous/bowlingscore/MainViewModelTest.kt
@@ -3,6 +3,7 @@ package com.anonymous.bowlingscore
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.google.common.truth.Truth.assertThat
 import io.mockk.spyk
+import io.mockk.verify
 import org.junit.Rule
 import org.junit.Test
 
@@ -20,5 +21,16 @@ class MainViewModelTest {
         viewModel.startNewGame()
 
         assertThat(viewModel.gameState.getValueForTest()?.frames).isEmpty()
+    }
+
+    @Test
+    fun `should return non empty frames after adding a roll`(){
+        val simulator: ScoreSimulator = spyk()
+
+        val viewModel = MainViewModel(simulator)
+
+        viewModel.addRoll()
+
+        assertThat(viewModel.gameState.getValueForTest()?.frames).isNotEmpty()
     }
 }
