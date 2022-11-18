86fbf3e513278676f10da390eba6dd3d6d8204de
rajesh.udhayan <70341492+rajesh-udhayan@users.noreply.github.com>
2022-11-18 13:42:15 +0530

    View model class created

--- app/src/main/java/com/anonymous/bowlingscore/MainActivity.kt
+++ app/src/main/java/com/anonymous/bowlingscore/MainActivity.kt
@@ -3,15 +3,17 @@ package com.anonymous.bowlingscore
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity: ComponentActivity() {
 
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
         }
     }
 }
