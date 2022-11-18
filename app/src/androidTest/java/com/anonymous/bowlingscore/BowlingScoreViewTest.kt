55f272b19a17d7443e85c2a753ef11cabf093394
rajesh.udhayan <70341492+rajesh-udhayan@users.noreply.github.com>
2022-11-18 13:56:20 +0530

    BowlingScoreView created in compose. Test passed for showing title.

---
+++ app/src/androidTest/java/com/anonymous/bowlingscore/BowlingScoreViewTest.kt
@@ -0,0 +1,36 @@
+package com.anonymous.bowlingscore
+
+import androidx.activity.viewModels
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.anonymous.bowlingscore.R.string.app_name
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+class BowlingScoreViewTest {
+
+    @get:Rule(order = 1)
+    internal val hiltTestRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 2)
+    internal val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Before
+    fun setUp(){
+        hiltTestRule.inject()
+        composeTestRule.setContent {
+            val viewModel = composeTestRule.activity.viewModels<MainViewModel>().value
+            BowlingScoreView(viewModel)
+        }
+    }
+
+    @Test
+    fun shouldDisplayAppTitle(){
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(app_name)).assertIsDisplayed()
+    }
+}
---
+++ app/src/main/java/com/anonymous/bowlingscore/BowlingScoreView.kt
@@ -0,0 +1,24 @@
+package com.anonymous.bowlingscore
+
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+
+@Composable
+fun BowlingScoreView(viewModel: MainViewModel){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name)
+                    )
+                }
+            )
+        }
+    ){
+
+    }
+}
